3308e589018c438f4359531af31b0a241fb18c02
ThibaudTraen <thibaud.traen@jopie.be>
2025-02-25 11:31:32 +0100

    Initial Commit

---
+++ boeksearchTests/boeksearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  boeksearchTests.swift
+//  boeksearchTests
+//
+//  Created by Thibaud Traen on 25/02/2025.
+//
+
+import Testing
+@testable import boeksearch
+
+struct boeksearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
