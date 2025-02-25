8e7bd21e75475bcae90b7537e99141fbd8748e48
ThibaudTraen <thibaud.traen@jopie.be>
2025-02-25 14:05:07 +0100

    update

--- boeksearch/ContentView.swift
+++ boeksearch/ContentView.swift
@@ -7,56 +7,14 @@
 
 import SwiftUI
 struct ContentView: View {
-    let trendingSearches = [
-        "gratis",
-        "will packer",
-        "susan morrison",
-        "the lexington letter",
-        "the body keeps the score",
-        "never split the difference",
-        "how to win friends & influence people",
-        "the 48 laws of power",
-        "if she knew",
-        "the you you are"
-    ]
-    
-    var body: some View {
-        NavigationStack{
-            VStack {
-                HStack {
-                    TextField("Books & Audiobooks", text: .constant(""))
-                        .padding(10)
-                        .background(Color(.systemGray6))
-                        .cornerRadius(10)
-                    
-                    Image(systemName: "mic.fill")
-                        .foregroundColor(.gray)
-                }.navigationTitle("Search")
-                    .padding(.horizontal).font(.custom("Serif", size: 24))
-                Spacer()
-                HStack{
-                    Text("Trending").font(.title3)
-                        .bold().padding([.top, .horizontal])
-                        .font(.custom("Serif", size: 20))
-                    Spacer()
-                }
-                List(trendingSearches, id: \..self) { search in
-                    Label(search, systemImage: "magnifyingglass").foregroundColor(.gray)
-                        .font(.custom("Serif", size: 18))
-                    
-                    
-                }.listStyle(PlainListStyle())
-                Text("Learn more about search result")
-                Spacer()
-            }
-        }
-    }
-}
-
-struct MainTabView: View {
     var body: some View {
         TabView {
-            ContentView()
+            Text("Home")
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+            
+            BookView()
                 .tabItem {
                     Label("Search", systemImage: "magnifyingglass")
                 }
@@ -74,14 +32,67 @@ struct MainTabView: View {
             Text("Audiobooks")
                 .tabItem {
                     Label("Audiobooks", systemImage: "headphones")
-                    
                 }
         }
     }
 }
-struct MainTabView_Previews: PreviewProvider {
-    static var previews: some View {
-        MainTabView()
+
+struct BookView: View {
+    let trendingSearches = [
+        "gratis",
+        "will packer",
+        "susan morrison",
+        "the lexington letter",
+        "the body keeps the score",
+        "never split the difference",
+        "how to win friends & influence people",
+        "the 48 laws of power",
+        "if she knew",
+        "the you you are"
+    ]
+    
+    var body: some View {
+        NavigationStack{
+            VStack {
+                HStack {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                        
+                        TextField("Books & Audiobooks", text: .constant(""))
+                            .padding(10)
+                            .background(Color(.systemGray6))
+                            .cornerRadius(10)
+                        
+                        Image(systemName: "mic.fill")
+                            .foregroundColor(.gray)
+                    }
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(12)
+                    .shadow(radius: 2)
+                }.navigationTitle("Search")
+                   .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                    .padding()
+                Spacer()
+                HStack{
+                    Text("Trending")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .padding()
+                    Spacer()
+                }
+                List(trendingSearches, id: \..self) { search in
+                    Label(search, systemImage: "magnifyingglass")
+                        
+                       
+                    
+                }.listStyle(PlainListStyle())
+                Text("Learn more about search result")
+                Spacer()
+            }
+        }
     }
 }
 
